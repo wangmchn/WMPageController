a27fff3b6b90458b8cc4ecb3e6910c40adac41a2
wangmchn <wangmchn@163.com>
2015-07-27 17:13:52 +0800

    update

--- WMPageController/WMPageConst.h
+++ WMPageController/WMPageConst.h
@@ -23,21 +23,6 @@
 //  导航菜单栏每个item的宽度
 #define WMMenuItemWidth 65
 //
-//  如需要利用观察者来订阅通知，请将 pageController 的 postNotification 设置为 YES
-//
-//  当一个控制器 init 完成且被添加成为子控制的时候会发布一个通知
-//  可用于判断当前控制器的序号，主要用于当同一个控制器管理时，通过判断序号来加载相应数据
-//  传递的数据包含两个信息，当前序号 (index) 以及标题 (title)
-//
-//  Note：由于在滚动中，上一个控制器还未被销毁，若两个相邻控制器都添加了观察，则都会收到通知。
-//  视图复用版本详见：https://github.com/wangmchn/YKPageView
-static NSString *const WMControllerDidFinishInitNotification = @"WMControllerDidFinishInitNotification";
-
-//  当一个控制器完全被展示在用户面前时发送的通知
-//  可用于判断当前控制器的序号，加载或者刷新当前数据.
-//  传递的数据包含两个信息，当前序号 (index) 以及标题 title
-static NSString *const WMControllerDidFullyDisplayedNotification = @"WMControllerDidFullyDisplayedNotification";
-//
 //
 //
 //
