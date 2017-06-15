bde0079c982e26de24cb2b31ac5ec1256674336e
Wang Ming <wangmchn@163.com>
2017-06-15 17:47:47 +0800

    Delete gesture control
    Signed-off-by: Wang Ming <wangmchn@163.com>

--- WMPageController/WMMenuView/WMScrollView.m
+++ WMPageController/WMMenuView/WMScrollView.m
@@ -8,19 +8,8 @@
 
 #import "WMScrollView.h"
 
-/// 其他所有 Pan 手势优先于 WMScrollView 的滚动手势
-/// 如想调整，自己处理冲突问题，(通过 UIGestureRecogizerDelegate 即可)
+/// 自己处理冲突问题，(通过 UIGestureRecogizerDelegate 即可)
 /// 删除 non-public 风险的 Api
 @implementation WMScrollView
-
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    if ([otherGestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]] &&
-        otherGestureRecognizer.state == UIGestureRecognizerStateBegan &&
-        [gestureRecognizer.view isKindOfClass:[WMScrollView class]] &&
-        [gestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
-        return YES;
-    }
-    return NO;
-}
 
 @end
