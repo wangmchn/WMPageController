347ef2e4d98b969536251b750de20fb36a357d64
Wang Ming <wangmchn@163.com>
2017-06-15 21:31:03 +0800

    Modified gesture compare (try avoid non-public api)
    Signed-off-by: Wang Ming <wangmchn@163.com>

--- WMPageController/WMMenuView/WMScrollView.m
+++ WMPageController/WMMenuView/WMScrollView.m
@@ -8,8 +8,29 @@
 
 #import "WMScrollView.h"
 
-/// 自己处理冲突问题，(通过 UIGestureRecogizerDelegate 即可)
-/// 删除 non-public 风险的 Api
 @implementation WMScrollView
+
+#pragma mark - <UIGestureRecognizerDelegate>
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    // iOS横向滚动的scrollView和系统pop手势返回冲突的解决办法:     http://blog.csdn.net/hjaycee/article/details/49279951
+    
+    // 兼容系统pop手势 / FDFullscreenPopGesture / 如有自定义手势，需自行对 Gesture 做判断
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UILayoutContainerView"]) {
+        if (otherGestureRecognizer.state == UIGestureRecognizerStateBegan && self.contentOffset.x == 0) {
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    //MARK: UITableViewCell 删除手势
+    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UITableViewWrapperView"] && [otherGestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
+        return YES;
+    }
+    return NO;
+}
 
 @end
