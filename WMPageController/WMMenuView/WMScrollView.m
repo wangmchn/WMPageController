cdd3ee47e1a1e832703368b302f6cb6d57b653c6
Wang Ming <wangmchn@163.com>
2017-07-05 21:02:31 +0800

    DELETE Gesture Simultaneously Method.
    Signed-off-by: Wang Ming <wangmchn@163.com>

--- WMPageController/WMMenuView/WMScrollView.m
+++ WMPageController/WMMenuView/WMScrollView.m
@@ -12,19 +12,6 @@
 
 #pragma mark - <UIGestureRecognizerDelegate>
 
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    // iOS横向滚动的scrollView和系统pop手势返回冲突的解决办法:     http://blog.csdn.net/hjaycee/article/details/49279951
-    
-    // 兼容系统pop手势 / FDFullscreenPopGesture / 如有自定义手势，需自行对 Gesture 做判断
-    if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UILayoutContainerView"]) {
-        if (otherGestureRecognizer.state == UIGestureRecognizerStateBegan && self.contentOffset.x == 0) {
-            return YES;
-        }
-    }
-    
-    return NO;
-}
-
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
     //MARK: UITableViewCell 删除手势
     if ([NSStringFromClass(otherGestureRecognizer.view.class) isEqualToString:@"UITableViewWrapperView"] && [otherGestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
